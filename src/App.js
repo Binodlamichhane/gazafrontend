710f3fa3f8c4fcabcd47323f233616e336aaf83e
BinodLamichhane <binodlamichhane9866@gmail.com>
2023-11-09 17:03:54 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect,useState} from 'react';
+import axios from 'axios';
 function App() {
+  const [data,setData]=useState([]);
+  const [name,setName]=useState('');
+  const [categories,setCategories]=useState('');
+  const [price,setPrice]=useState(0);
+  useEffect(()=>{
+    axios.get('http://localhost:3700/api/product/myproduct')
+    .then((res)=>{setData(res.data);
+      console.log("response from api",res.data);})
+    .catch((error)=>{console.log("error while fetching data",error)})
+  })
+  const handleNameChange=(event)=>{
+    setName(event.target.value);
+  }
+  const handlePriceChange=(event)=>{
+    setPrice(event.target.value);
+  }
+  const handleCategoriesChange=(event)=>{
+    setCategories(event.target.value)
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const datacombine={
+      name:name,
+      price:price,
+      categories:categories
+    }
+    axios.post('http://localhost:3700/api/product/addproduct',datacombine)
+    .then((res)=>{console.log('data send to exprees successfullu')})
+    .catch(()=>{console.log('error while sending data to express');});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>this is front end page </p>
+        <div>
+          {/* {data.map((subdata)=>{
+            return <div>
+              <h1>{subdata.name}</h1>
+              <h2>{subdata.categories}</h2>
+              <h3>{subdata.price}</h3>
+            </div>
+          })} */}
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label for='name'>Name:</label>
+            <input name='name' type='text' value={name} onChange={handleNameChange}/><br/>
+            <label for='price'>Price:</label>
+            <input name='price' type='number' value={price} onChange={handlePriceChange}/><br/>
+            <label for='cate'>Categories:</label>
+            <input name= 'cate' type='text' value={categories} onChange={handleCategoriesChange}/><br/>
+            <input type='submit' value='submit'/>
+          </form>
+        </div> 
     </div>
   );
 }
